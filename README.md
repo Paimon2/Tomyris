f41945efe32c6c0a9a8f58ceed5afa0776f91792
Omar <omarjunaid22@gmail.com>
2019-12-08 20:20:40 +0800

    Added bare essentials incl. beta installer

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Tomyris
-An intelligent camera-based alarm system with a web interface
+Named after an ancient Messagetean queen, Tomyris is an intelligent camera-based alarm system with a web interface. 
+
+## Installation
+
+Ensure Python 3.7 or newer is installed. Older versions may work, but are unsupported.
+MySQL 15.0 or newer is recommended for the database.
+
+On Linux, run `./install.sh` with root privileges if you wish to install Tomyris as a systemd service.
+
+On other operating systems, or if you're rebellious/can't use systemd, get ready; things are a little rougher.
+
+You'll need to run ``python tomyris_core.py`` for as long as you want Tomyris to operate (hold up - see below!).
+
+If you're not using the install script, run ``python tomyris_core.py`` once to allow it to generate the configuration file.
+Then, fill in `config.txt` with your database information.
---
+++ install.sh
@@ -0,0 +1,53 @@
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run with root permissions." 
+   exit 1
+fi
+
+echo -e "Installing Tomyris as a daemon. The process will begin in 5 seconds.\nCTRL+C to cancel."
+sleep 5
+echo "Cloning Tomyris into /opt (1/6)..."
+cd /opt
+git clone https://github.com/UltraFuture7000/Tomyris tomyris
+echo "Installing requirements (2/6)..."
+pip install -r /opt/tomyris/requirements.txt
+echo "Installing systemd service (3/6)..."
+cp /opt/tomyris/tomyris.service /etc/systemd/system
+chmod 644 /etc/systemd/system/tomyris.service
+echo "Setting up systemd service (4/6)..."
+
+read -p "Automatically start Tomyris on boot (y/n)?" persistentstart
+if [[ $persistentstart =~ ^(yes|y| ) ]] || [[ -z $persistentstart ]]; then
+  systemctl enable tomyris;
+  echo "Tomyris will be automatically started at boot."
+fi
+
+echo "Installing tomyrisctl (5/6)..."
+cp /opt/tomyris/tomyrisctl.py /usr/bin
+mv /usr/bin/tomyrisctl.py /usr/bin/tomyrisctl
+chmod a+x /usr/bin/tomyrisctl
+echo "Configuring Tomyris (6/6)..."
+# configparser will be used to read these files
+
+
+read -p "What's the host of your database?" dbhost
+read -p "What's the port of your database (if unsure, type 3306)?" dbport
+read -p "What user should we use to access your database?" dbuser
+read -p "What's the password of that user?" dbpassword
+read -p "And finally, what's the name of your database?" dbname
+
+echo -e "[DATABASE]\n" >> /opt/tomyris/config.txt
+echo -e "Host = $dbhost\n" >> /opt/tomyris/config.txt
+echo -e "Port = $dbport\n" >> /opt/tomyris/config.txt
+echo -e "User = $dbuser\n" >> /opt/tomyris/config.txt
+echo -e "Password = $dbpassword\n" >> /opt/tomyris/config.txt
+echo -e "Name = $dbname\n" >> /opt/tomyris/config.txt
+
+
+read -p "Start Tomyris now (y/n)?" persistentstart
+if [[ $persistentstart =~ ^(yes|y| ) ]] || [[ -z $persistentstart ]]; then
+  systemctl start tomyris;
+  echo "Tomyris has been started."
+fi
+
+echo "Tomyris has been installed successfully!"
+echo "View https://github.com/UltraFuture7000/Tomyris/blob/master/README.md for help."
