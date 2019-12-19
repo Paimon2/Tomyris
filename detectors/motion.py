fd0f3ee09888be4e6833106b21706499300a8982
UltraFuture7000 <omarjunaid22@gmail.com>
2019-12-19 14:18:33 +0800

    Fixed motion detectors state

---
+++ detectors/motion.py
@@ -0,0 +1,47 @@
+"""
+The source code below consists of collated and modified snippets from:
+https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+"""
+
+"""
+--START DETECTOR SETTINGS--
+
+Modify the values below to as you see fit.
+"""
+
+
+min_contour_area = 40  # Minimum area of contour to classify as high-confidence
+
+# --END DETECTOR SETTINGS--
+
+import time
+import numpy as np
+import imutils
+import cv2
+import datetime
+import imutils
+import cv2
+
+
+def preprocess_frame(frame):
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    return gray
+
+def get_confidence_score(prev, curr):
+    prev = preprocess_frame(prev)
+    curr = preprocess_frame(curr)
+    # Compute the diff between prev and curr
+    frame_delta = cv2.absdiff(prev, curr)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    for c in cnts:
+        if cv2.contourArea(c) < min_contour_area:
+            continue
+        return 1  # Always return max value; motion can't be expressed a %?
---
+++ tests/motion.py
@@ -0,0 +1,30 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+import cv2
+from detectors import motion
+
+
+def get_path():
+    return os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+
+
+class TestMotionDetectorFunctionality(unittest.TestCase):
+
+    def test_motion_detector(self):
+        one = cv2.imread(get_path() + "motion1.png")
+        two = cv2.imread(get_path() + "motion2.png")
+        result = motion.get_confidence_score(one, two)
+        self.assertEqual(result, 1)
+
+    def test_no_motion(self):
+        one = cv2.imread(get_path() + "motion1.png")
+        two = cv2.imread(get_path() + "motion1.png")
+        result = motion.get_confidence_score(one, two)
+        self.assertEqual(result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
