824ce8f14fd867207093ed28f38d8730dbda4f79
UltraFuture7000 <omarjunaid22@gmail.com>
2019-12-20 15:58:38 +0800

    Actually added test for detector

---
+++ tests/yolov3tiny.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+import cv2
+from detectors import yolov3_tiny
+
+def get_path():
+    return os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+class TestDatabaseFunctionality(unittest.TestCase):
+
+    def test_people(self):
+        frame = cv2.imread(get_path() + "cars1.png")
+        objs = yolov3_tiny.get_objects_of_interest(frame)
+        self.assertGreater(len(objs), 4)
+
+if __name__ == '__main__':
+    unittest.main()
